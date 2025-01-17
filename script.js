125e9f5c5714ed5773cec59ef617ab9da1de6100
TheWoholo <lakkimsettisreyas@gmail.com>
2025-01-17 15:43:29 +0000

    Add getComputerChoice Function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+console.log("Hello World");
+
+function getComputerChoice(){
+    const options = ["Rock","Paper","Scissors"];
+    let choice = Math.floor(Math.random()*3);
+    console.log(options[choice]);
+    return options[choice];
+    
+}
+
