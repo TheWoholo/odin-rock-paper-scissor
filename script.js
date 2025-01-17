30752eeed86817b66bfd62aebcfffbabe014b8fa
TheWoholo <lakkimsettisreyas@gmail.com>
2025-01-17 16:10:51 +0000

    Add playRound Logic

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ function getHumanChoice(){
 
 let humanScore=0;
 let computerScore=0;
+
+function playRound(humanChoice,computerChoice){
+    const map = new Map();
+    map.set("rock",0);
+    map.set("paper",1);
+    map.set("scissor",2);
+    let humanNum = map.get(humanChoice);
+    let computerNum = map.get(computerChoice);
+    if(humanNum==computerNum)
+    {
+        console.log("Tied!");
+    }
+    else if(humanNum==(computerNum+1)%3)
+    {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+    else
+    {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+    console.log(humanScore);
+    console.log(computerScore);
+}
+
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice().toLowerCase();
+
+playRound(humanChoice,computerChoice);
