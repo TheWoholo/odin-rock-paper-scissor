7d157abed6fa7e396767f54f7a26a8f0b5d51cfe
TheWoholo <lakkimsettisreyas@gmail.com>
2025-01-17 16:16:19 +0000

    Play 5 rounds

--- script.js
+++ script.js
@@ -12,36 +12,43 @@ function getHumanChoice(){
     return prompt("What do you choose?");
 }
 
-let humanScore=0;
-let computerScore=0;
+function playGame(){
 
-function playRound(humanChoice,computerChoice){
+    let humanScore=0;
+    let computerScore=0;
     const map = new Map();
     map.set("rock",0);
     map.set("paper",1);
     map.set("scissor",2);
-    let humanNum = map.get(humanChoice);
-    let computerNum = map.get(computerChoice);
-    if(humanNum==computerNum)
-    {
-        console.log("Tied!");
+
+    function playRound(humanChoice,computerChoice){
+        let humanNum = map.get(humanChoice);
+        let computerNum = map.get(computerChoice);
+        if(humanNum==computerNum)
+        {
+            console.log("Tied!");
+        }
+        else if(humanNum==(computerNum+1)%3)
+        {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+        else
+        {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
     }
-    else if(humanNum==(computerNum+1)%3)
-    {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
-    }
-    else
-    {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+
+    for(let i=0;i<5;i++){
+        const humanChoice = getHumanChoice().toLowerCase();
+        const computerChoice = getComputerChoice().toLowerCase();
+        playRound(humanChoice,computerChoice);
     }
 
     console.log(humanScore);
     console.log(computerScore);
+
 }
 
-const humanChoice = getHumanChoice().toLowerCase();
-const computerChoice = getComputerChoice().toLowerCase();
-
-playRound(humanChoice,computerChoice);
+playGame();
